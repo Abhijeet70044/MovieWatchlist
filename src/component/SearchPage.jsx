d980f29519832184bc68d24533c699b1dd30505c
Abhijeet <Abhijeet Kumar>
2024-10-10 21:17:39 +0530

    MoviecardUpdated

--- src/component/SearchPage.jsx
+++ src/component/SearchPage.jsx
@@ -1,30 +1,79 @@
-// src/components/SearchPage.js
-import React, { useState } from "react";
-import SearchBar from "./SearchBar";
-import MovieCard from "./MovieCard";
-import omdbApi from "../Api/OpenMovieApi"; // Import the API file
+  import React, { useState } from "react";
+  import SearchBar from "./SearchBar";
+  import MovieCard from "./MovieCard";
+  import omdbApi from "../Api/OpenMovieApi"; // Import the API file
+  import "./SearchPage.css"; // Import CSS
 
-import "./SearchPage.css"; // Import CSS
-
-const SearchPage = () => {
+  const SearchPage = () => {
   const [movies, setMovies] = useState([]);
-
+  const [watchList, setWatchList] = useState([]); // Watch list state
   const handleSearch = async (query) => {
     const response = await omdbApi.searchMovies(query); // Use the API
     setMovies(response);
-  };
-
-  return (
+    };
+    // Add movie to watch list
+    const addToWatchList = (movie) => {
+    if (!watchList.find((m) => m.imdbID === movie.imdbID)) {
+    setWatchList([...watchList, movie]);
+    }
+    };
+    // Remove movie from watch list
+    const removeFromWatchList = (movie) => {
+    setWatchList(watchList.filter((m) => m.imdbID !== movie.imdbID));
+    };
+    // Add all movies from the current search result to the watch list
+    const addAllToWatchList = () => {
+    const newMovies = movies.filter(
+    (movie) => !watchList.find((m) => m.imdbID === movie.imdbID)
+    );
+    setWatchList([...watchList, ...newMovies]);
+    };
+    return (
     <div className="search-page">
-      <h2 className="page-title">Search for Movies</h2>
-      <SearchBar onSearch={handleSearch} />
-      <div className="movie-grid">
-        {movies.map((movie) => (
-          <MovieCard key={movie.imdbID} movie={movie} />
-        ))}
-      </div>
-    </div>
+    <h2 className="page-title">Search for Movies</h2>
+    <SearchBar onSearch={handleSearch} />
+    {/* Add All to Watch List Button */}
+    {movies.length > 0 && (
+    <button className="add-all-button" onClick={addAllToWatchList}>
+    Add All to Watch List
+    </button>
+    )}
+    {/* Movies Grid */}
+    <div className="movie-grid">
+    {movies.map((movie) => {
+    const isInWatchList = watchList.some(
+      (watchMovie) => watchMovie.imdbID === movie.imdbID
   );
-};
-
-export default SearchPage;
+  return (
+  <MovieCard
+  key={movie.imdbID}
+  movie={movie}
+  isInWatchList={isInWatchList} // Pass if movie is in the watch list
+  onAddToWatchList={() => addToWatchList(movie)} // Pass add function
+  onRemoveFromWatchList={() => removeFromWatchList(movie)} // Pass remove
+  function
+  />
+  );
+  })}
+  </div>
+  {/* Watch List Section */}
+  {watchList.length > 0 && (
+  <div className="watch-list">
+  <h3>Your Watch List</h3>
+  <div className="movie-grid">
+  {watchList.map((movie) => (
+  <MovieCard
+  key={movie.imdbID}
+  movie={movie}
+  isInWatchList={true} // In the watch list, always true
+  onRemoveFromWatchList={() => removeFromWatchList(movie)} // Allow
+  removal
+  />
+  ))}
+  </div>
+  </div>
+  )}
+  </div>
+  );
+  };
+  export default SearchPage;
